1d20d38bde62eb23140cd6fd988fe5598ff77dd4
notbeloser <a850710a@gmail.com>
2017-06-08 15:16:46 +0800

    Opencv VR two camera capture

---
+++ src/DisplayImage.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(){
+    VideoCapture video1(0);
+    VideoCapture video2(1);
+
+    namedWindow("video",CV_WINDOW_NORMAL);
+    setWindowProperty("video", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN );
+    Mat Frame1,Frame2;
+
+    while(true){
+        video1 >> Frame1;
+        video2 >> Frame2;
+        Mat C;
+        hconcat(Frame2,Frame2,C);
+        imshow("video",C);
+       int k = waitKey(33);
+       if(k=='a')
+    	   break;
+    }
+    return 0;
+}
