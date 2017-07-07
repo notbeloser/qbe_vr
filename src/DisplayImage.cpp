41e670015a2b3f28003254bcf0dfa60a5d717594
notbeloser <a850710a@gmail.com>
2017-07-07 14:34:28 +0800

    Measure Time add

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -1,24 +1,32 @@
-#include <cstdio>
+#include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <time.h>
 using namespace cv;
 
 int main(){
-    VideoCapture video1(1);
-    VideoCapture video2(2);
+    VideoCapture video1(0);
+    VideoCapture video2(1);
 
     namedWindow("video",CV_WINDOW_NORMAL);
     setWindowProperty("video", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN );
     Mat Frame1,Frame2;
-
+    clock_t start,stop;
     while(true){
+	start = clock();
         video1 >> Frame1;
         video2 >> Frame2;
+	stop = clock();
+	printf("Loading Time %fs\n",double(stop - start) / CLOCKS_PER_SEC);
+	start = clock();
         Mat C;
         hconcat(Frame1,Frame2,C);
+	stop = clock();
+	printf("Merge Time %fs\n",double(stop - start) / CLOCKS_PER_SEC);
         imshow("video",C);
-       int k = waitKey(33);
-       if(k=='a')
-    	   break;
+       	int k = waitKey(1);
+      	if(k=='a')
+    		break;
+	system("clear");
     }
     return 0;
 }
---
+++ src/openvr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash#
+export DISPLAY=:0
+DISPLAY=:0 xdotool getactivewindow key Down
+./VR
+exit 0
